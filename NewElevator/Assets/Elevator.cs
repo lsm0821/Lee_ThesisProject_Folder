2997bce5aef45cdf89347aa104751edb8fd050db
lsm0821 <lsm02289@gmail.com>
2021-02-05 15:43:16 -0800

    Thesis Project New Repository
    test

---
+++ NewElevator/Assets/Elevator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+
+    public float speed = 2;
+
+    Vector3 initialLocation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialLocation = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().MovePosition(transform.position + new Vector3(0, speed / 1000, 0));
+        //transform.position += new Vector3(0, speed / 100, 0);
+    }
+}
---
+++ NewElevator/Assets/MessageListener.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageListener : MonoBehaviour
+{
+    int midValue = 192;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMessageArrived(string message)
+    {
+        Debug.Log(message);
+
+        /*if (midValue == 192)
+        {
+            midValue = System.Convert.ToInt32(message);
+        }*/
+
+        //GetComponent<Elevator>().speed = midValue - System.Convert.ToInt32(message);
+        GetComponent<Elevator>().speed = System.Convert.ToInt32(message);
+
+    }
+
+    void OnConnectionEvent(bool success)
+    {
+        Debug.Log(success ? "Device Connected" : "Device disconnected");
+    }
+}
